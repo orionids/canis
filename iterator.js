547cd89d23bf1df6afc637478ad4e85564fb95a1
adaptiveflow <adaptiveflow@gmail.com>
2018-01-10 01:52:47 +0900

    Several pending options added

--- iterator.js
+++ iterator.js
@@ -9,38 +9,60 @@ module.exports = class {
 		this.index = -1;
 		// iterator only sends two cases for progress,
 		// one is multi-level iteration was fully completed ( state == undefined )
-		// another is multi-level loop is terminated by RETURN ( state == RETURN )
+		// another is multi-level loop is terminated by TERMINATE ( state == TERMINATE )
 		this.progress = progress;
 	}
 
 	add( n, c, cb, pop ) {
-		var l = this.stack.length;
 		var t = ++this.index;
-		if ( t >= l ) this.stack.push( {} );
-		this.stack[t] = {
-			"context" : c,
-			"index" : 0,
-			"total" : n,
-			"callback" : cb,
-			"pop" : pop
-		};
+		if ( t >= this.stack.length ) this.stack.push( {} );
+		var e = this.stack[t];
+		e.context = c;
+		e.index = 0;
+		e.total = n;
+		e.callback = cb;
+		e.pop = pop;
 	}
 
-	run() {
+	// end can be called instead of run after pending
+	end() {
+		this.progress( this, module.exports.TERMINATE );
+		this.index = -1;
+	}
+
+	run( brk ) {
 		while ( this.index >= 0 ) {
 			var e = this.stack[this.index];
-			for (;;) {
-				var i = e.index;
-				if ( i >= e.total ) break;
-				e.index = i + 1;
-				var result = e.callback( this, e.context, i );
-				if ( result == module.exports.PENDING ) return;
-				if ( result == module.exports.BREAK ) break;
-				if ( result == module.exports.RETURN ) {
-					this.progress( this, module.exports.RETURN );
-					return;
+			switch ( e.state ) {
+				case module.exports.PENDING_BREAK: brk = true; break;
+				case module.exports.PENDING_END: this.end(); return;
+			}
+			if ( brk != true ) {
+				for (;;) {
+					var i = e.index;
+					if ( i >= e.total ) break;
+					e.index = i + 1;
+					var result = e.callback( this, e.context, i );
+					if ( result == module.exports.BREAK ) {
+						break;
+					} else {
+						switch ( result ) {
+							case module.exports.PENDING:
+							return;
+							case module.exports.PENDING_BREAK:
+							e.state = module.exports.PENDING_BREAK;
+							return;
+							case module.exports.PENDING_END:
+							e.state = module.exports.PENDING_END;
+							return;
+							case module.exports.END:
+							this.end();
+							return;
+						}
+					}
 				}
 			}
+			e.context = undefined;
 			this.index --;
 			if( e.pop !== undefined ) e.pop( this, e.context );
 		}
@@ -51,5 +73,7 @@ module.exports = class {
 
 module.exports.CONTINUE = 0;
 module.exports.PENDING = 1;
+module.exports.PENDING_BREAK = 2;
+module.exports.PENDING_END = 3;
 module.exports.BREAK = -1;
-module.exports.RETURN = -2;
+module.exports.END = -2;
