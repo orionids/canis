490914d49362a45bc7c3e3838869d5a78cb4aa5b
adaptiveflow <adaptiveflow@gmail.com>
2018-02-21 00:32:11 +0900

    On demand symbol resolve

---
+++ test/resolve.js
@@ -0,0 +1,22 @@
+var server = require("canis/server")
+
+
+var sym = {
+	"?" : function( ctx, s ) {
+		console.log( "On demand resolve" );
+		if ( s == "world" ) return sym.world = ctx.result;
+	},
+	"??" : {
+		result : ", world"
+	},
+	"hello": "Hello",
+}
+
+var ctx = {
+	i : 0,
+	delim : { open:"{", close:"}", escape:"\\"}
+};
+
+console.log( server.resolve( "{hello}{world}", [ sym ] , ctx ) );
+console.log( sym.world );
+console.log( server.resolve( "{hello}{world}", [ sym ] , ctx ) );
