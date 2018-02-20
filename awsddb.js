069040384ea749d6e0e9411e6462ec2703ac1743
adaptiveflow <adaptiveflow@gmail.com>
2018-02-21 00:18:14 +0900

    update expression & delete queried results

--- awsddb.js
+++ awsddb.js
@@ -1,34 +1,102 @@
 // Copyright (c) 2018, adaptiveflow
 // Distributed under ISC
 
-'use strict'
+'use strict';
 
-exports.hashKeyQuery = function( ddbcli, tblname, key, val, callback ) {
+exports.partitionKeyQuery = function( ddbcli, tblname, key, val, callback )
+{
 	ddbcli.query( {
 		TableName: tblname,
-		KeyConditionExpression: "#key=:val",
+		KeyConditionExpression: "#k=:v",
 		ExpressionAttributeNames: {
-			"#key" : key
+			"#k" : key
 		},
 		ExpressionAttributeValues: {
-			":val" : val
+			":v" : val
 		}
 	}, callback );
-}
+};
 
-/*
 exports.primaryKeyQuery = function
-	( ddbcli, tblname, hashkey, hashval, sortkey, sortval, cond, callback ) {
+	( ddbcli, tblname, key, val, sortkey, sortval, extra, cond, callback )
+{
+	var c = "#k=:v and " + ( cond == null ?
+		"begins_with(#sk,:sv)" : "#sk" + cond + ":sv" );
+	if ( extra ) c += "and :x";
 	ddbcli.query( {
 		TableName: tblname,
-		KeyConditionExpression: "#hk=:hk and begins_with(#sk,:sv)",
+		KeyConditionExpression: c,
 		ExpressionAttributeNames: {
-			"#hk" : hashkey,
-			"#sk" : sorttkey,
+			"#k" : key,
+			"#sk" : sortkey,
 		},
 		ExpressionAttributeValues: {
-			":hv" : hashval,
+			":v" : val,
 			":sv" : sortval,
+			":x" : extra
 		}
-	}, function(err,data) {
-}*/
+	}, callback );
+};
+
+exports.updateExpression = function( param, action, state, name, value )
+{
+	var cmd;
+	var expr = "#" + name;
+	if ( param.ExpressionAttributeNames === undefined )
+		param.ExpressionAttributeNames = {};
+	param.ExpressionAttributeNames[expr] = name;
+	if ( value !== undefined ) {
+		if ( param.ExpressionAttributeValues === undefined )
+			param.ExpressionAttributeValues = {};
+		param.ExpressionAttributeValues[":" + name] = value;
+	}
+	if ( action == state ) expr = "," + expr;
+	switch ( action ) {
+		case 0: cmd = "set"; expr += "=:" + name; break;
+		case 1 : cmd = "add"; expr += " :" + name; break;
+		case -1: cmd = "delete"; break;
+		default: cmd = "remove";
+	}
+	if ( action != state ) {
+		if ( param.UpdateExpression === undefined ) param.UpdateExpression = "";
+		param.UpdateExpression += cmd + expr;
+	} else {
+		param.UpdateExpression +=expr;
+	}
+	return action;
+};
+
+// delete queried result
+exports.delete = function( ddbcli, tblname, data, key, cond, callback )
+{
+	if ( data ) {
+		var item = data.Items;
+		if ( item ) {
+			var param = {
+				TableName: tblname,
+			};
+			var i = 0;
+			(function del(e,d) {
+				for (;;) {
+					if ( i < item.length ) {
+						var di = data.Items[i++];
+						if ( cond ) {
+							if ( !cond(di) ) continue;
+						}
+						param.Key = {};
+						for ( var j = 0; j < key.length; j++ ) {
+							var k = key[j];
+							param.Key[k] = di[k];
+						}
+						ddbcli.delete( param, del );
+					} else {
+						callback( e, d);
+					}
+					break;
+				}
+			})();
+			return;
+		}
+	}
+	callback( null, data );
+};
