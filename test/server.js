3c6cc16aac7e7be836dd2d837ddbd2172322bc85
adaptiveflow <adaptiveflow@gmail.com>
2018-05-27 16:54:19 +0900

    Interactive console and clients finalization

--- test/server.js
+++ test/server.js
@@ -1,1 +1,24 @@
-require( "canis/server.js" ).main( "testapi" );
+var server = require( "canis/server.js" );
+
+console.log( "Type quit() to terminate server" );
+var s = server.main ( "testapi",
+
+// this parameter is optional
+{
+	parse: // suppy this for interactive mode
+	function(input,quit){
+		try {
+			eval(input);
+		} catch ( e ) {
+			console.log( e );
+			quit(false);
+		}
+	},
+	finalize: function() {
+		server.close( s );
+		console.log( "Bye~" );
+	},
+	port : 5000, // default is 3000
+	client: true // manage clients
+} );
+
